344fc3cb6e5faa3cf6832eedd4b1add2e4bdfb14
Tije13 <Knuppeltje2009@gmail.com>
2021-07-14 09:20:47 +0200

    Update youtube.js

--- commands/youtube.js
+++ commands/youtube.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     
-    message.channel.send("https://www.youtube.com/channel/UCY5ukP65M1tXeR_QhIIOmKg")
+    return message.channel.send("https://www.youtube.com/channel/UCY5ukP65M1tXeR_QhIIOmKg");
 
 }
 
