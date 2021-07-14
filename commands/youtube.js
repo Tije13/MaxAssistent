8ddd916c47d073f65ee655d6f7e509cc11a0fa85
Tije13 <Knuppeltje2009@gmail.com>
2021-07-14 09:19:37 +0200

    Create youtube.js

---
+++ commands/youtube.js
@@ -0,0 +1,11 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    
+    message.channel.send("https://www.youtube.com/channel/UCY5ukP65M1tXeR_QhIIOmKg")
+
+}
+
+module.exports.help = {
+    name: "youtube"
+}
